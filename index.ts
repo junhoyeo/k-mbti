1058d8ad609d7bd910d01516f71b7fbd305872c5
Junho Yeo <hanaro0704@gmail.com>
2021-07-01 12:37:20 +0900

    feat: implement function `pronounceMBTI`

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import Hangul from 'hangul-js'
+
 export type MBTI = `${'I' | 'E'}${'S' | 'N'}${'T' | 'F'}${'J' | 'P'}`
 
 type Split<Str extends string, Separator extends string> =
@@ -10,3 +12,46 @@ type Split<Str extends string, Separator extends string> =
           : [Str]
 
 export type MBTIArray = Split<MBTI, ''>
+
+export const pronounceMBTI = (mbti: MBTI) => {
+  const mbtiArray = mbti.split('') as MBTIArray
+
+  const [
+    ExtraversionOrIntroversion,
+    SensingOrIntuition,
+    ThinkingOrFeeling,
+    JudgingOrPerceiving,
+  ] = mbtiArray
+
+  let result = []
+  if (ExtraversionOrIntroversion ==='E') {
+    result.push('ㅇ')
+    result.push('ㅔ')
+  } else {
+    result.push('ㅇ')
+    result.push('ㅣ')
+  }
+
+  if (SensingOrIntuition ==='N') {
+    result.push('ㄴ')
+  } else {
+    result.push('ㅅ')
+  }
+
+  if (ThinkingOrFeeling ==='T') {
+    result.push('ㅌ')
+    result.push('ㅣ')
+  } else {
+    result.push('ㅍ')
+    result.push('ㅣ')
+  }
+
+  if (JudgingOrPerceiving ==='P') {
+    result.push('ㅂ')
+  } else {
+    result.push('ㅈ')
+    result.push('ㅔ')
+  }
+
+  return Hangul.assemble(result)
+}
