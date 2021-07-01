f66f57700be44c29de9cbf4cf8fea1a3667c9bde
Junho Yeo <hanaro0704@gmail.com>
2021-07-01 12:35:13 +0900

    feat: implement type `MBTIArray`

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
 export type MBTI = `${'I' | 'E'}${'S' | 'N'}${'T' | 'F'}${'J' | 'P'}`
+
+type Split<Str extends string, Separator extends string> =
+    string extends Str
+      ? string[]
+      : Str extends ''
+        ? []
+        : Str extends `${infer PartOne}${Separator}${infer PartTwo}`
+          ? [PartOne, ...Split<PartTwo, Separator>]
+          : [Str]
+
+export type MBTIArray = Split<MBTI, ''>
